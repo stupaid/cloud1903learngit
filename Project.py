b25b2f00fe806b2e522aecfb1674f50a3f0a5810
mj163 <mengjianzuishuai@qq.com>
2019-06-17 10:22:37 +0800

    write python script.

---
+++ Project.py
@@ -0,0 +1,11 @@
+str01 = 'hi! i can sleep.'
+str02 = "this's definition a string type variable"
+str03 = '''
+    this line one.
+    this line two.
+    this line three.
+'''
+
+print(str01)
+print(str02)
+print(str03)
